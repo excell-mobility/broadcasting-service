a3b767d6a24af6895172aa7dfa02f2f8ea4dabfb
Andre Rademacher <andre.rademacher.business@googlemail.com>
2017-03-10 10:54:23 +0000

    Enable phpunit!

---
+++ test/BroadcastingTest/Action/HomePageActionTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace BroadcastingTest\Action;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Short description for class
+ *
+ * Long description for class
+ *
+ * @package   Broadcasting Service
+ * @author    André Rademacher <andre.rademacher@entiretec.com>
+ * @copyright Copyright (c) 2017 ENTIRETEC (http://www.entiretec.com)
+ * @license   ENTIRETEC proprietery license
+ */
+class HomePageActionTest extends TestCase
+{
+    public function testAlwaysSuccessful(): void
+    {
+        $this->assertTrue(true);
+    }
+}
